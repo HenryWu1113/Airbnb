579dbd290f09a6d08b08ea51dd4fc4edc2e1c0e3
HenryWu1113 <bug910284@gmail.com>
2023-12-18 01:40:19 +0800

    步驟二一半(3:57:00)

--- app/Components/Modals/RentModal.tsx
+++ app/Components/Modals/RentModal.tsx
@@ -5,6 +5,8 @@ import useRentModal from '@/app/hooks/useRentModal'
 import Modal from './Modal'
 import Heading from '../Heading'
 import CategoryInput from '../Inputs/CategoryInput'
+import CountrySelect from '../Inputs/CountrySelect'
+import Map from '../../Components/Map'
 
 import { useForm, FieldValues } from 'react-hook-form'
 import { useMemo, useState } from 'react'
@@ -46,11 +48,12 @@ const RentModal = () => {
     }
   })
 
-  /** 監聽 category 的值可以做是否被選取的判斷 */
-  const category = watch('category')
+  /** 監聽目前的值(可以做是否被選取的判斷) */
+  const category: string = watch('category')
+  const location: any = watch('location')
 
   const setCustomValue = (id: string, value: any) => {
-    console.log(id)
+    // console.log(id)
     console.log(value)
     // 因為 react-hook-form 的 setValue 不會重新渲染頁面，因此包在 function 裡面
     setValue(id, value, {
@@ -114,11 +117,31 @@ const RentModal = () => {
     </div>
   )
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div
+        className='
+    flex flex-col gap-8
+    '
+      >
+        <Heading
+          title='Where is your place located?'
+          subtitle='Help guests find you!'
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue('location', value)}
+        />
+        <Map />
+      </div>
+    )
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
